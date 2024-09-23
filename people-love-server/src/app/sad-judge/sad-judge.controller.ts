97c0d8373cb5547b5b65c949c2a4efe1629ac4c2
Kimhyunmook <gusanr4200@naver.com>
2024-09-23 15:57:53 +0900

    remove app dir

--- people-love-server/src/app/sad-judge/sad-judge.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { SadJudgeService } from './sad-judge.service';
-
-@Controller('sad-judge')
-export class SadJudgeController {
-  constructor(private readonly sadJudgeService: SadJudgeService) {}
-}
--- people-love-server/src/app/sad-judge/sad-judge.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SadJudgeService } from './sad-judge.service';
-import { SadJudgeController } from './sad-judge.controller';
-
-@Module({
-  controllers: [SadJudgeController],
-  providers: [SadJudgeService],
-})
-export class SadJudgeModule {}
